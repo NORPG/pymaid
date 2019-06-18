dafe4643e99053ae69ed4005960d3a5d1698410b
CCHung <b10102229@gmail.com>
2019-06-18 09:26:37 +0000

    initial commit  Changes to be committed: 	modified:   .gitignore 	new file:   LICENSE 	new file:   apple.jpg 	new file:   img_bld.py 	new file:   img_bld_hw.py 	new file:   orange.jpg

---
+++ img_bld.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np,sys
+it = 6
+A = cv2.imread('apple.jpg', cv2.IMREAD_GRAYSCALE)
+B = cv2.imread('orange.jpg', cv2.IMREAD_GRAYSCALE)
+# generate Gaussian pyramid for A
+G = A.copy()
+gpA = [G]
+for i in xrange(it):
+    G = cv2.pyrDown(G)
+    gpA.append(G)
+# generate Gaussian pyramid for B
+G = B.copy()
+gpB = [G]
+for i in xrange(it):
+    G = cv2.pyrDown(G)
+    gpB.append(G)
+# generate Laplacian Pyramid for A
+lpA = [gpA[it-1]]
+for i in xrange((it-1),0,-1):
+    GE = cv2.pyrUp(gpA[i])
+    L = cv2.subtract(gpA[i-1],GE)
+    lpA.append(L)
+# generate Laplacian Pyramid for B
+lpB = [gpB[it-1]]
+for i in xrange((it-1),0,-1):
+    GE = cv2.pyrUp(gpB[i])
+    L = cv2.subtract(gpB[i-1],GE)
+    lpB.append(L)
+# Now add left and right halves of images in each level
+LS = []
+for la,lb in zip(lpA,lpB):
+    rows,cols = la.shape
+    ls = np.hstack((la[:,0:cols/2], lb[:,cols/2:]))
+    LS.append(ls)
+# now reconstruct
+ls_ = LS[0]
+for i in xrange(1,it):
+    ls_ = cv2.pyrUp(ls_)
+    ls_ = cv2.add(ls_, LS[i])
+# image with direct connecting each half
+real = np.hstack((A[:,:cols/2],B[:,cols/2:]))
+cv2.imwrite('Pyramid_blending2.jpg',ls_)
+cv2.imwrite('Direct_blending.jpg',real)
